9b7601f4fadbb2364fdae881a4b0d87550f09c26
wchen <weijie.chen@elliemae.com>
2017-08-16 16:21:16 -0700

    change

---
+++ leetcode-297-hard/Codec.java
@@ -0,0 +1,32 @@
+
+
+import java.util.*;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+/**
+ * Serialize and Deserialize binary tree
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.deserialize(codec.serialize(root));
